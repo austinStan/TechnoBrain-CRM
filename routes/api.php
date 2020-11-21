c6eb324e1305a39ebac8a158c454c1f957a40ef0
austinStan <austinstan24@gmail.com>
2020-11-21 11:43:51 +0300

    Update Auth registration API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("user-signup", "UserController@userSignUp");
+
+Route::post("user-login", "UserController@userLogin");
+
+Route::get("user/{email}", "UserController@userDetail");
