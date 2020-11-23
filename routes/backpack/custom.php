36fcf5437423656cce48ec90f07707915a514e79
austinStan <austinstan24@gmail.com>
2020-11-23 14:25:59 +0300

    Update Agents and Company CRUD

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -15,4 +15,6 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::crud('customers', 'CustomersCrudController');
+    Route::crud('agents', 'AgentsCrudController');
+    Route::crud('company', 'CompanyCrudController');
 }); // this should be the absolute last line of this file
