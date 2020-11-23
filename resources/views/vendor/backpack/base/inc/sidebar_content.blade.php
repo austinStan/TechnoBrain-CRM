ed5d17ff12483d292272748dbda8baa929f045a3
austinStan <austinstan24@gmail.com>
2020-11-23 14:31:45 +0300

    Update Icons Added For the Backend Sideview

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('customers') }}'><i class='nav-icon la la-globe'></i> Customers</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('agents') }}'><i class='nav-icon la la-question'></i> Agents</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('company') }}'><i class='nav-icon la la-question'></i> Companies</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('agents') }}'><i class='nav-icon la la-school'></i> Agents</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('company') }}'><i class='nav-icon la la-building '></i> Companies</a></li>
